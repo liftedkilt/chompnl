de72bf03f847e357ce76caa9995fdc09c42126a2
William Forsyth <william.forsyth@liferay.com>
2020-01-16 08:50:38 -0800

    Iterate over multiple files

--- chompnl.go
+++ chompnl.go
@@ -9,15 +9,31 @@ import (
 )
 
 func main() {
-
 	if len(os.Args) <= 1 {
-		fmt.Println("Please pass the name of the file to chomp.")
+		fmt.Println("Please pass the names of the file to chomp.")
 
 		os.Exit(1)
 	}
 
-	fn := os.Args[1]
+	filesChanged := 0
 
+	for i := 1; i < len(os.Args); i++ {
+		fn := os.Args[i]
+
+		nlchars := chompfile(fn)
+
+		if nlchars > 0 {
+			filesChanged++
+			log.Println(fn + ": Removed " + strconv.Itoa(nlchars) + " newline(s).")
+		}
+	}
+
+	if filesChanged > 0 {
+		log.Fatalln("Removed newlines from " + strconv.Itoa(filesChanged) + " files.")
+	}
+}
+
+func chompfile(fn string) int {
 	b, err := ioutil.ReadFile(fn) // b has type []byte
 	if err != nil {
 		log.Fatal(err)
@@ -38,7 +54,5 @@ func main() {
 
 	ioutil.WriteFile(fn, out, 0644)
 
-	if nlchars > 0 {
-		log.Fatalln(fn + ": Removed " + strconv.Itoa(nlchars) + " newline(s).")
-	}
+	return nlchars
 }
